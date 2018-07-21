ef8218ef36ac3a17841f980f3ca6bac0424f2c65
Alefe Souza <contact@alefesouza.com>
2018-07-21 15:56:38 -0300

    fix: fix Babel use with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const gulp = require('gulp');
 
 const csso = require('gulp-csso');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
 const htmlMin = require('gulp-htmlmin');
+
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
 
 const paths = {
   html: 'public/**/*.html',
   css: 'public/css/**/*.css',
   js: 'public/js/**/*.js',
+  jsEntry: 'public/js/index.js',
 };
 
 gulp.task('views', () => gulp.src(paths.html)
@@ -21,17 +27,31 @@ gulp.task('styles', () => gulp.src(paths.css)
   .pipe(csso())
   .pipe(gulp.dest('dist/css')));
 
-gulp.task('scripts', () => gulp.src(paths.js)
-  .pipe(babel({
-    presets: ['env'],
-  }))
-  .pipe(uglify())
-  .pipe(gulp.dest('dist/js')));
+gulp.task('scripts', function () {
+  const b = browserify({
+    entries: paths.jsEntry,
+    debug: true,
+    transform: [babelify.configure({
+      presets: ['env'],
+      plugins: ['transform-runtime']
+    })]
+  });
 
-gulp.task('dist', ['styles', 'scripts', 'views']);
+  return b.bundle()
+    .pipe(source('index.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({
+      loadMaps: true
+    }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('dist', ['scripts', 'styles', 'views']);
 
 gulp.task('default', () => {
-  gulp.watch(paths.html, ['views']);
-  gulp.watch(paths.css, ['styles']);
   gulp.watch(paths.js, ['scripts']);
+  gulp.watch(paths.css, ['styles']);
+  gulp.watch(paths.html, ['views']);
 });
